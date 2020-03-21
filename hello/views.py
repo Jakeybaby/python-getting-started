ad12790c6ec10083cb061b860413e4e41c934661
Jakeybaby <43561941+Jakeybaby@users.noreply.github.com>
2020-03-22 12:17:02 +1300

    te

--- hello/views.py
+++ hello/views.py
@@ -8,7 +8,7 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     times = int(os.environ.get('TIMES',114))
-    return HttpResponse('CCC! ' * times)
+    return HttpResponse('CCC1! ' * times)
 
 
 def db(request):

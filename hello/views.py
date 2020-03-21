f1f1efdd808a3806a4e57e531197f57b9964ca60
Jakeybaby <43561941+Jakeybaby@users.noreply.github.com>
2020-03-22 11:44:33 +1300

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -7,7 +7,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    times = int(os.environ.get('TIMES',4))
+    times = int(os.environ.get('TIMES',14))
     return HttpResponse('Hello! ' * times)
 
 
